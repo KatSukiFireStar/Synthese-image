8dceac94002b007ac622ad2f01668c38a43040c5
KaTSukiFireStar <78344193+KatSukiFireStar@users.noreply.github.com>
2024-10-22 10:02:58 +0200

    Update Ressort.py

--- SimulationPhysique/Ressort.py
+++ SimulationPhysique/Ressort.py
@@ -16,6 +16,7 @@ def update(frame):
 
 
 pointAccroche = Vec3(0,10,0)
+pointAccroche2 = Vec3(-4,10,0)
 sizeRope = 2
 
 p0 = Vec3(-2, 10, 0)
@@ -23,33 +24,43 @@ v0 = Vec3(0, 0, 0)
 g = 10
 m = 1
 dt = 0.01
-k = 10
+k = 100
 attenuation = 0.9
+coef = 0.315
 
+F_d = - coef * v0.norm() * v0
 F_g = Vec3(0, -1 * m * g, 0)
 a = Vec3(0,0,0)
 l = p0.distance(pointAccroche)
+l2 = p0.distance(pointAccroche2)
 res = k * (l - sizeRope)
-F_r = res * (pointAccroche - p0)
+res2 =  k * (l2 - sizeRope)
+F_r1 = res * (pointAccroche - p0)
+F_r2 = res * (pointAccroche2 - p0)
 
 t = 0 + dt
 p_dt = p0
 p = p0 + dt * v0
-v = v0 + dt * (F_g + F_r) / m
+v = v0 + dt * (F_g + F_r1 + F_r2 + F_d) / m
 
-while t < 100:
+while t < 100 :
     x.append(p.x)
     y.append(p.y)
 
     l = p.distance(pointAccroche)
+    l2 = p.distance(pointAccroche2)
     res = k * (l - sizeRope)
-    F_r = res * (pointAccroche - p)
-    a: Vec3 = (F_g + F_r) / m
+    res2 = k * (l2 - sizeRope)
+    F_r1 = res * (pointAccroche - p)
+    F_r2 = res * (pointAccroche2 - p)
+    F_d = - coef * v.norm() * v
+    a: Vec3 = (F_g + F_r1 + F_r2 + F_d) / m
 
-    # p = p + dt * v
-    (p_dt, p) = (p, 2 * p - p_dt + (dt * dt) * a)
-    v = (p - p_dt) / dt
-    # v = v + dt * a
+
+    # (p_dt, p) = (p, 2 * p - p_dt + (dt * dt) * a)
+    # v = (p - p_dt) / dt
+    v = v + dt * a
+    p = p + dt * v
 
     t = t + dt
 
